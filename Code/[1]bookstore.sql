e3b26bb01026144c51dc174a23a6893fec81ccfe
JyopsZ <timmyyap39@gmail.com>
2025-07-27 22:32:55 +0800

    Placed admin grants

--- Code/[1]bookstore.sql
+++ Code/[1]bookstore.sql
@@ -192,12 +192,12 @@ CREATE TABLE IF NOT EXISTS Product_Archive (
 -- -----------------------------------------------------
 -- USERS
 -- -----------------------------------------------------
--- CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'staff'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'customer'@'localhost' IDENTIFIED BY 'password';
 
 -- ADMIN
--- GRANT ALL PRIVILEGES ON bookstore.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON bookstore.* TO 'admin'@'localhost';
 SELECT user, host FROM mysql.user;
 -- STAFF
 GRANT ALL PRIVILEGES ON bookstore.Products TO 'staff'@'localhost';
