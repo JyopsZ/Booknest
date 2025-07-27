326336afc8ffc471b38bdb64cb6bf3356265ec5f
JyopsZ <timmyyap39@gmail.com>
2025-07-27 23:00:09 +0800

    Updated customer privileges

--- Code/[1]bookstore.sql
+++ Code/[1]bookstore.sql
@@ -206,8 +206,9 @@ GRANT SELECT ON bookstore.Order_Items TO 'staff'@'localhost';
 GRANT SELECT ON bookstore.Transaction_Log TO 'staff'@'localhost';
 
 -- CUSTOMER
-GRANT SELECT, INSERT ON bookstore.User_Balance TO 'customer'@'localhost';
-GRANT SELECT, INSERT ON bookstore.User_Balance_Load TO 'customer'@'localhost';
+GRANT ALL PRIVILEGES ON bookstore.User_Balance TO 'customer'@'localhost';
+GRANT ALL PRIVILEGES ON bookstore.User_Balance_Load TO 'customer'@'localhost';
 GRANT SELECT, INSERT ON bookstore.Orders TO 'customer'@'localhost';
 GRANT SELECT, INSERT ON bookstore.Order_Items TO 'customer'@'localhost';
+GRANT SELECT ON bookstore.Products TO 'customer'@'localhost';
 FLUSH PRIVILEGES;
